8b44070dddde18d2d23f9f3a9a2187c688c51a1d
Amina880 <dkalekwa37@gmail.com>
2024-08-01 18:22:53 +0300

    initial commit

---
+++ dom-manipulation/script.js
@@ -0,0 +1,20 @@
+//Write a JavaScript file (script.js) that handles the creation and manipulation of DOM elements based on user interactions.
+//Manage an array of quote objects where each quote has a text and a category.
+//Implement functions to display a random quote and to add new quotes called showRandomQuote and createAddQuoteForm` respectively
+
+const quoteDisplay = document.getElementById('quoteDisplay')
+
+let quoteArray = [];
+
+const quoteObject = {
+    quoteText: quoteObj,
+    category: categoryObj
+} 
+
+function showRandomQuote(){
+const quote = document.createElement('p')
+}
+
+function createAddQuoteForm(){
+
+}
