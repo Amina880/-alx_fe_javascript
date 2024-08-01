128dab026dfc863cdaf7efa608c2e4bee7f4dc5a
Amina880 <dkalekwa37@gmail.com>
2024-08-01 23:40:06 +0300

    added functions

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -3,6 +3,10 @@
 //Implement functions to display a random quote and to add new quotes called showRandomQuote and createAddQuoteForm` respectively
 
 const quoteDisplay = document.getElementById('quoteDisplay')
+const displayButton = document.getElementById('newQuote')
+const newquoteInput = document.getElementById('newQuoteText')
+const newcategoryInput = document.getElementById('newQuoteCategory')
+
 
 let quoteArray = [];
 
@@ -11,10 +15,33 @@ const quoteObject = {
     category: categoryObj
 } 
 
-function showRandomQuote(){
-const quote = document.createElement('p')
+function displayRandomQuote(){
+
+if(quoteArray.length === 0){
+    quoteDisplay.innerHTML = '<P>No quotes available. Please add some!</P>'
+    return;
+}
+const randomIndex = Math.floor(Math.random()* quoteArray.length);
+const selectedQuote = quoteArray[randomIndex];
+quoteDisplay.textContent =  `"${selectedQuote.text}" - Category: ${selectedQuote.category}`
 }
 
-function createAddQuoteForm(){
 
+function createAddQuoteForm(event){
+    event.preventdefault();
+    const newquoteText = newquoteInput.value.trim();
+    const newcategory = newcategoryInput.value.trim();
+    
+    if(newquoteText === '' || newcategory === ''){
+    alert('Both fields are required');
+    return;
+    }
+    const newQuote = {
+        quoteText: newquoteText,
+        category: newcategory
+    }
+    quoteArray.push(newQuote);
+    newquoteInput.value = '';
+    newcategoryInput.value = '';
+    alert("Quote added successfully!")
 }
